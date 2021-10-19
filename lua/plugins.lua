233b8923872029506402e4acdf06ff9ef8d1f49e
Alan Jui <AlanJui.1960@gmail.com>
2021-10-19 20:39:29 +0800

    在 Plugins 管理工具，加入 Bootstrapping 功能：automatically install and set up packer.nvim on any machine when you clone neovim configuration to

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,13 @@
 -- This file can be loaded by calling `lua require('plugin_name')
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local packer_bootstrap
 
-return require('packer').startup(function()
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
@@ -9,4 +16,10 @@ return require('packer').startup(function()
   use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
   use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
