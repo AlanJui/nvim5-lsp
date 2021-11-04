2221bf253d2490144e8615c22ab5c161c7a44abf
Alan Jui <AlanJui.1960@gmail.com>
2021-11-04 11:47:56 +0800

    藉由 Neovim nvim-lsp-installer 套件之助，變更各 Language Server 之啟動作法

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@
 -- =======================================================================
 local USER_HOME_PATH = os.getenv('HOME')
 local PYENV_ROOT_PATH = USER_HOME_PATH .. '/.pyenv'
-local PYENV_GLOBAL_PATH = PYENV_ROOT_PATH .. '/versions/VENV-397'
+local PYENV_GLOBAL_PATH = PYENV_ROOT_PATH .. '/versions/venv-397'
 local PYTHON_BINARY = PYENV_GLOBAL_PATH .. '/bin/python3'
 
 local g = vim.g -- a table to access global variables
--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,19 +1,53 @@
 -- =======================================================================
 -- Language Server Configuration
 -- =======================================================================
+local lspconfig = require('lspconfig')
+local lsp_installer_servers = require('nvim-lsp-installer.servers')
+
+local server_available, requested_server = lsp_installer_servers.get_server('sumneko_lua')
+if server_available then
+	requested_server:on_ready(function()
+		lspconfig.sumneko_lua.setup({
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { 'vim' }
+					},
+				},
+			},
+		})
+	end)
+
+	if not requested_server:is_installed() then
+		-- Queue the server to be installed
+		requested_server:install()
+	end
+end
+
+-- Provide settings first!
+local lsp_installer = require('nvim-lsp-installer')
+lsp_installer.settings({
+	ui = {
+		icons = {
+			server_installed = '✓',
+			server_pending = '⁉',
+			server_uninstalled = ''
+		}
+	}
+})
 
 
 -- Language Server Setup
 --------------------------------------------------------------------------
 
 -- Setup Language Server for Bash
-require('lspconfig').bashls.setup{}
+-- require('lspconfig').bashls.setup{}
 
 -- Setup Language Server for Lua
-require('lsp.lsp-lua')
+-- require('lsp.lsp-lua')
 
 -- Setup LS for Python / TypeScript / C && C++
-require('lsp.lsp-misc')
+-- require('lsp.lsp-misc')
 
 -- TypeScript
 -- require('lsp.lsp-typescript')
