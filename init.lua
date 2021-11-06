3d375a4742201b33b407ae3c9a320c02d4d149de
Alan Jui <AlanJui.1960@gmail.com>
2021-11-06 22:05:32 +0800

    改用 nvim-lsp-installer 套件，設定各 LSP servers 的啟動；原先用 lspconfig['LSP_Server_Name'].setup({}) ，現在改用 lsp_installer.on_server_ready(function(server) ... end)

--- init.lua
+++ init.lua
@@ -49,6 +49,3 @@ vim.cmd([[ set number relativenumber ]])
 vim.opt.textwidth=0
 vim.opt.wrapmargin=0
 vim.wo.wrap = false
-function name()
-	-- code
-end
--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,7 +1,33 @@
 -- =======================================================================
--- Language Server Configuration
+-- LSP Servers Installation
 -- =======================================================================
 -- require('lsp.nvim-lsp-installer')
+-- Automatically install LSP servers
+local lsp_installer = require('nvim-lsp-installer')
+local servers = {
+	'sumneko_lua',
+	'pyright',
+	'html',
+	'emmet_ls',
+	'tsserver',
+	'vuels',
+	'yamlls',
+	'bashls',
+}
+for _, name in pairs(servers) do
+	local ok, server = lsp_installer.get_server(name)
+	-- Check that the server is supported in nvim-lsp-installer
+	if ok then
+		if not server:is_installed() then
+			print('Installing ' .. name)
+			server:install()
+		end
+	end
+end
+
+-- =======================================================================
+-- Language Server Configuration
+-- =======================================================================
 local lspconfig = require('lspconfig')
 local util = require('lspconfig.util')
 local cmp = require('cmp')
@@ -116,115 +142,129 @@ cmp.setup.cmdline(':', {
 -- Setup Language Server
 --------------------------------------------------------------------------
 
--- Python(Pyright) Language Server
-lspconfig.pyright.setup({
-    cmd = { "pyright-langserver", "--stdio" },
-    filetypes = { "python" },
-    root_dir = function(fname)
-        local root_files = {
-            'pyproject.toml',
-            'setup.py',
-            'setup.cfg',
-            'requirements.txt',
-            'Pipfile',
-            'pyrightconfig.json',
-        }
-        return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
-    end,
-    settings = {
-		python = {
-			analysis = {
-				autoSearchPaths = true,
-				diagnosticMode = "workspace",
-				useLibraryCodeForTypes = true,
-			},
-		},
-    },
-	on_attach = on_attach,
-	capabilities = capabilities,
-})
+lsp_installer.on_server_ready(function (server)
+	-- Specify the default options which we'll use for all LSP servers
+	local default_opts = {
+		on_attach = on_attach,
+		capabilities = capabilities,
+	}
 
--- HTML Language Server
-lspconfig.html.setup({
-    cmd = { "vscode-html-language-server", "--stdio" },
-	filetypes = {
-		"html",
-		"htmldjango",
-		"css",
-	},
-    init_options = {
-      configurationSection = { "html", "htmldjango", "css", "javascript" },
-      embeddedLanguages = {
-        css = true,
-        javascript = true
-      },
-    },
-	root_dir = function(fname)
-	  return util.root_pattern('package.json', '.git')(fname) or util.path.dirname(fname)
-	end,
-    settings = {},
-	on_attach = on_attach,
-	capabilities = capabilities,
-})
+	-- Create a server_opts table where we'll specify our custom LSP server configuration
+	local server_opts = {
+		['pyright'] = function ()
+			default_opts.cmd = { "pyright-langserver", "--stdio" }
+			default_opts.filetypes = { "python" }
+			default_opts.on_attach = on_attach
+			default_opts.capabilities = capabilities
 
--- Emmet Language Server
-lspconfig.emmet_ls.setup({
-	cmd = { 'emmet-ls', '--stdio' },
-	filetypes = {
-		'html',
-		'htmldjango',
-		'css',
-	},
-	root_dir = util.root_pattern('package.json', '.git'),
-	-- root_dir = function (fname)
-	-- 	return vim.loop.cwd()
-	-- end,
-	settings = {},
-	on_attach = on_attach,
-	capabilities = capabilities,
-})
-vim.g.completion_trigger_character = { '.' }
+			return default_opts
+		end,
 
--- Lua Language Server
--- require('lsp.lsp-lua')
-local USER_HOME_PATH = os.getenv('HOME')
--- local LUA_INSTALL_PATH = USER_HOME_PATH .. '/.local/share/nvim/lsp_servers/sumneko_lua'
--- local sumneko_root_path = LUA_INSTALL_PATH .. '/extension/server/bin/' .. system_name
-local LUA_INSTALL_PATH = USER_HOME_PATH .. '/.local/share/lua-language-server'
-local sumneko_root_path = LUA_INSTALL_PATH
-local sumneko_binary = sumneko_root_path .. '/bin/' .. system_name .. '/lua-language-server'
+		['tsserver'] = function ()
+			default_opts.cmd = {
+				'typescript-language-server',
+				'--stdio',
+			}
 
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
+			default_opts.filetypes = {
+				'javascript',
+				'javascriptreact',
+				'typescript',
+				'typescriptreact',
+			}
 
-lspconfig.sumneko_lua.setup({
-    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
-    settings = {
-        Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-                -- Setup your lua path
-                path = runtime_path,
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = {'vim'},
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-        		library = vim.api.nvim_get_runtime_file("", true),
-			},
-			-- Do not send telemetry data containing a randomized but unique identifier
-			telemetry = {
-			  enable = false,
-			},
-        },
-    },
-	on_attach = on_attach,
-	capabilities = capabilities,
-})
+			default_opts.on_attach = on_attach
+			default_opts.capabilities = capabilities
+
+			return default_opts
+		end,
+
+		['html'] = function ()
+			default_opts.cmd = { "vscode-html-language-server", "--stdio" }
+			default_opts.filetypes = {
+				"html",
+				"htmldjango",
+				"css",
+			}
+			default_opts.on_attach = on_attach
+			default_opts.capabilities = capabilities
+
+			return default_opts
+		end,
+
+		['emmet_ls'] = function ()
+			default_opts.cmd = { "emmet-ls", "--stdio" }
+			default_opts.filetypes = {
+				"html",
+				"htmldjango",
+				"css",
+			}
+			default_opts.on_attach = on_attach
+			default_opts.capabilities = capabilities
+
+			return default_opts
+		end,
+
+		['sumneko_lua'] = function ()
+			-- bin: $HOME/.local/share/nvim/lsp_servers/sumneko_lua/extensions/bin/$OS/lua-language-server
+			-- root: $HOME/.local/share/nvim/lsp_servers/sumneko_lua/extensions/bin/$OS/main.lua
+			local lua_root_path = vim.fn.stdpath('data') .. '/lsp_servers/sumneko_lua/extension/server/bin/' .. system_name
+			local lua_binary = lua_root_path .. '/lua-language-server'
+
+			-- bin: $HOME/.local/share/lua-language-server/bin/$OS/lua-language-server
+			-- root: $HOME/.local/share/lua-language-server/main.lua
+			-- local USER_HOME_PATH = os.getenv('HOME')
+			-- local LUA_INSTALL_PATH = USER_HOME_PATH .. '/.local/share/lua-language-server'
+			-- local lua_root_path = LUA_INSTALL_PATH
+			-- local lua_binary = lua_root_path .. '/bin/' .. system_name .. '/lua-language-server'
+
+			local runtime_path = vim.split(package.path, ';')
+			table.insert(runtime_path, 'lua/?.lua')
+			table.insert(runtime_path, 'lua/?/init.lua')
+
+			default_opts.cmd = {
+				lua_binary,
+				'-E',
+				lua_root_path .. '/main.lua',
+			}
+
+			default_opts.settings = {
+				Lua = {
+					runtime = {
+						-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+						version = 'LuaJIT',
+						-- Setup your lua path
+						path = runtime_path,
+					},
+					diagnostics = {
+						-- Get the language server to recognize the `vim` global
+						globals = {'vim'},
+					},
+					workspace = {
+						-- Make the server aware of Neovim runtime files
+						library = vim.api.nvim_get_runtime_file("", true),
+					},
+					-- Do not send telemetry data containing a randomized but unique identifier
+					telemetry = {
+					  enable = false,
+					},
+				},
+			}
+
+			default_opts.on_attach = on_attach
+			default_opts.capabilities = capabilities
+			return default_opts
+		end,
+	}
+
+	-- We check to see if any custom server_opts exist for the LSP server, if so, load them,
+	-- if not, use our default_opts
+	server:setup(server_opts[server.name] and server_opts[server.name]() or default_opts)
+	vim.cmd([[
+		" do User LspAttachBuffers
+	]])
+end)
+
 
 -- Snippets Engine
 --------------------------------------------------------------------------
